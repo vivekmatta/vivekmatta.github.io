474a2b102576f00c51a9082b3d84738092530296
vivekmatta <vmatta2002@gmail.com>
2025-07-29 21:51:41 -0400

    Add static build files for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/vivekmatta.github.io' : '',
 };
 
 export default nextConfig;
